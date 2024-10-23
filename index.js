7778f5979a106a2b27089afad953c3e308f73e96
George Howard <110793196+cpugz@users.noreply.github.com>
2024-10-23 16:06:14 -0500

    init index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello")
+var strGlobal = "This is a global variable"
+const strConstant = "This is a constant"
+let strScoped = "This is a scoped variable"
+
+console.log(strGlobal)
+console.log(strConstant)
+console.log(strScoped)
