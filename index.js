d031fb3c13979420da642f8ce8e580ac069c75cf
George Howard <110793196+cpugz@users.noreply.github.com>
2024-10-23 16:18:40 -0500

    app starts and listens on port 8000

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-console.log("Hello")
-var strGlobal = "This is a global variable"
-const strConstant = "This is a constant"
-let strScoped = "This is a scoped variable"
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql")
+const {v4:uuidv4} = require("uuid")
 
-console.log(strGlobal)
-console.log(strConstant)
-console.log(strScoped)
+const HTTP_PORT = 8000
+
+var app = express()
+app.use(cors())
+app.use(express.json())
+
+app.listen(HTTP_PORT, () => {
+    console.log("App listening on port", HTTP_PORT)
+})
