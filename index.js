7bed038510dde6f9ef99d23fc6d805d25c37beea
George Howard <110793196+cpugz@users.noreply.github.com>
2024-10-23 16:27:42 -0500

    first working backend that runs on localhost:8000

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ var app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/hello", (req, res, next) => {
+    res.status(200).json({message:"Hello World"})
+})
+
+app.get("/goodbye", (req, res, next) => {
+    res.status(200).json({message:"Goodbye World"})
+})
+
 app.listen(HTTP_PORT, () => {
     console.log("App listening on port", HTTP_PORT)
 })
